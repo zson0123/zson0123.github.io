5aa179a0553eac6740699718e9e3e543686553da
zson0123 <zacheryyson@gmail.com>
2020-07-15 02:18:00 +0900

    Add files via upload
    add projects and type feature. Adjust css styling

--- index.js
+++ index.js
@@ -10,29 +10,27 @@ function formValidation() {
     }
 }
 
+// // To Top Button
+// var upButton = document.getElementById("upBtn");
 
-// To Top Button
-upButton = document.getElementById("upBtn");
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     upButton.style.display = "block";
+//   } else {
+//     upButton.style.display = "none";
+//   }
+// }
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    upButton.style.display = "block";
-  } else {
-    upButton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
+// // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
 // Coming Soon
 function comingSoon() {
-    alert("Feature coming soon.");
+    alert("Feature coming soon. Email me at zacheryyson@gmail.com!");
 }
